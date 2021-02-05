22e974d713f53c11b76f95cbb20d1daf1384fb6c
F1reFinger <helderbonzinho@gmail.com>
2021-02-05 19:03:30 -0300

    create: Menu e validador de data

---
+++ src/codigos/app.java
@@ -0,0 +1,37 @@
+package codigos;
+
+import java.util.Scanner;
+
+public class app {
+    public static void main(String args[]){
+        int escolha = 10;
+        ValidaData validador = new ValidaData();
+
+
+        while(escolha != 0) {
+            System.out.println("Escolha a seguir o que deseja:");
+            System.out.println("1 - Entrada de Temoeratura.");
+            System.out.println("2 - Calculo de Temperatura media.");
+            System.out.println("3 - calculo de temperatura minima.");
+            System.out.println("4 - Calculo de temperatura maxima.");
+            System.out.println("5 relatorio meteorologico.");
+            System.out.println("0 - Sair");
+
+            Scanner leitura = new Scanner(System.in);
+            escolha = leitura.nextInt();
+
+
+            if(escolha < 0 || escolha > 5){
+                System.out.println("Não consigo identificar sua escolha");
+            }else{
+                switch (escolha){
+                    case 1:
+                        validador.Validetor();
+                }
+
+            }
+
+        }
+    }
+}
+
